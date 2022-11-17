ccd3101edfd74f486bcad35eb7db130e8c20c161
Joshua Ferris <jajjferris@gmail.com>
2022-11-17 09:47:14 -0500

    :sparkles: app shell and router

---
+++ src/views/NotFound.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="flex-1 w-full flex justify-center items-center">
+    <div class="px-4 sm:px-6 md:px-8 lg:px-10 py-8 w-50 max-w-sm flex flex-col bg-white shadow-md rounded-3xl">
+      <h1 class="text-xl sm:text-2xl md:text-3xl lg:text-4xl text-center">404</h1>
+      <p class="text-xs sm:text-sm md:text-md lg:text-lg text-gray-600 text-center">Page not found</p>
+      <router-link to="/" class="text-sm sm:text-md md:text-lg lg:text-xl text-emerald-600 hover:text-emerald-700 text-center">Click to return to safety</router-link>
+    </div>
+  </div>
+</template>
